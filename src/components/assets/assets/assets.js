c5b1f6f2596d7437f63e9f2891596c3d0e3c993e
sivakumar <siva.ms2000@gmail.com>
2024-08-07 14:49:53 -0400

    assets

---
+++ src/components/assets/assets/assets.js
@@ -0,0 +1,130 @@
+import basket_icon from './basket_icon.png'
+
+import search_icon from './search_icon.png'
+
+import mark1 from './reads1.jpg'
+import mark2 from './read2.jpg'
+import mark3 from './read3.jpeg'
+import mark4 from './read4.jpeg'
+
+import food_1 from './product3.jpeg'
+import food_2 from './product5.jpeg'
+import food_3 from './product.jpeg'
+import food_4 from './product4.jpeg'
+import food_5 from './product2.jpeg'
+
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    basket_icon,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Comfort Glide Stroller",
+        image: food_1,
+        price: 70,
+        description: "Enjoy smooth and safe strolls with our Comfort Glide Stroller, designed for ultimate comfort and convenience.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Gentle Baby Lotion",
+        image: food_2,
+        price: 30,
+        description: "Nourish your baby's delicate kin with our gentle and hydrating baby lotion, perfect for daily use. ",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Smoothing Pacifer",
+        image: food_3,
+        price: 15,
+        description: "Keep our baby calm with our soothing pacificer, designed for maximum comfort and easy grip.",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Cozy Baby Blankets",
+        image: food_4,
+        price: 40,
+        description: " Wrap your little one in our ultra-soft, cozy baby blankets, providing warmth and comfort all day long.",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Playful Baby Toy",
+        image: food_5,
+        price: 15,
+        description: "Engage your baby's senses with tou, designed to stimulate development and provide endless fun.",
+        category: "Rolls"
+    }
+]
+
+export const market_list = [
+    {
+        _id: "1",
+        name: "Recognizing Early Pregnancy Symptoms: What to Look For and When to Test",
+        image: mark1,
+        //price: +91-9042428014,
+        description: "Early pregnancy symptoms can include a missed period, frequent urination, fatigue, morning sickness, and tender breasts, while less common signs are spotting, cravings, metallic taste, headaches, cramps, mood swings, congestion, bloating, and skin changes. ",
+        category: "Salad",
+        web: "https://en.wikipedia.org/wiki/BTS"
+    },
+    {
+        _id: "2",
+        name: "Understanding the First Trimester: Common Pregnancy Symptoms",
+        image: mark2,
+        //price: +91-9041112825,
+        description: "Feeling unusually tired or queasy? Learn about the common symptoms in the first trimester of pregnancy and how your body is working hard to support your growing baby. Feeling unusually tired or queasy? Learn about the common symptoms in the first trimester of pregnancy.",
+        category: "Salad",
+         web: "https://en.wikipedia.org/wiki/BTS"
+    }, {
+        _id: "3",
+        name: "Pregnancy Nutrition: Eating for Two",
+        image: mark3,
+       // price: +91-8108138000,
+        description: "Discover the myths and facts about nutrition during pregnancy, including how much you really need to eat and the best foods to support your baby’s growth. Learn why balanced meals, rich in protein, calcium, and iron, are essential for a healthy pregnancy​ ",
+        category: "Salad",
+         web: "https://en.wikipedia.org/wiki/BTS"
+    }, {
+        _id: "4",
+        name: "The Stages of Pregnancy",
+        image: mark4,
+        price: "https://rb.gy/ft4mqu",
+        description: "Pregnancy is a journey of transformation for both the baby and the mother, marked by three trimesters of growth and change. Each stage brings unique developments and challenges, from the early formation of vital organs in the first trimester to the rapid growth.",
+        category: "Salad",
+        web: "https://en.wikipedia.org/wiki/BTS",
+    }
+]
